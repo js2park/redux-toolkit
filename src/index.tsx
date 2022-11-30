8453325395eb6bf1343a3c5cf8e4a6bda98a10fa
juseoung <js.park@qbgroup.co.kr>
2022-11-30 23:55:51 +0900

    [fix] lint error

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,9 @@ import App from './App';
 import { store } from './store';
 import { Provider } from 'react-redux';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(
+	document.getElementById('root') as HTMLElement,
+);
 root.render(
 	<Provider store={store}>
 		<App />
